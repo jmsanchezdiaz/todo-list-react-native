2a8f4c17fa57b7df1bbe9d3974894d7eb4c0c33e
jmsanchezdiaz <jmsanchezdiaz02@gmail.com>
2023-02-24 02:09:43 -0300

    todo app with expo and rnative

--- App.js
+++ App.js
@@ -1,20 +1,154 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  Dimensions,
+  FlatList,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  Touchable,
+  TouchableHighlight,
+  View
+} from "react-native";
 
 export default function App() {
+  const [items, setItems] = useState(new Map([[1, { id: 1, name: "Leche" }]]));
+  const [inputText, setInputText] = useState("");
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  const updateProduct = () => {
+    if (!inputText || !items.has(selectedProduct.id)) return;
+
+    const newItems = new Map(items);
+
+    newItems.set(selectedProduct.id, { ...selectedProduct, name: inputText });
+
+    setItems(newItems);
+    setInputText("");
+    setSelectedProduct(null);
+  };
+
+  const addProduct = () => {
+    if (!inputText || items.has(inputText)) return;
+    const newProduct = {
+      id: items.size,
+      name: inputText
+    };
+
+    const newItems = new Map(items);
+
+    newItems.set(newProduct.id, newProduct);
+
+    setItems(newItems);
+    setInputText("");
+  };
+
+  const removeProduct = (id) => {
+    if (!id || !items.has(id)) return;
+
+    const newItems = new Map(items);
+
+    newItems.delete(id);
+
+    setItems(newItems);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Mi Lista de Compra</Text>
+      <View>
+        <TextInput
+          value={inputText}
+          onChangeText={setInputText}
+          placeholder="Ingrese su producto..."
+          style={styles.input}
+          defaultValue={inputText}
+        />
+        <View style={{ marginHorizontal: 15 }}>
+          <Button
+            onPress={selectedProduct ? updateProduct : addProduct}
+            title={selectedProduct ? "Actualizar" : "Añadir"}
+            accessibilityLabel="añadir or actualizar un producto a la lista"
+          />
+        </View>
+        <FlatList
+          style={{ margin: 15 }}
+          data={Array.from(items.values())}
+          renderItem={({ item }) => (
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginVertical: 10
+              }}>
+              <Text>{item.name}</Text>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center"
+                }}>
+                <TouchableHighlight
+                  onPress={(e) => removeProduct(item.id)}
+                  style={{
+                    backgroundColor: "#f00",
+                    padding: 10,
+                    marginHorizontal: 5
+                  }}>
+                  <Text
+                    style={{
+                      color: "white",
+                      fontWeight: "bold"
+                    }}>
+                    Eliminar
+                  </Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                  onPress={() => {
+                    setSelectedProduct(item);
+                    setInputText(item.name);
+                  }}
+                  style={{
+                    backgroundColor: "#00f",
+                    padding: 10,
+                    marginHorizontal: 5
+                  }}>
+                  <Text style={{ color: "white", fontWeight: "bold" }}>
+                    Seleccionar
+                  </Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightgrey",
+    height: Dimensions.get("screen").height
   },
+  heading: {
+    fontSize: 30,
+    fontWeight: "bold",
+    backgroundColor: "#FFC107",
+    color: "#555",
+    paddingTop: 30,
+    paddingHorizontal: 20,
+    paddingBottom: 10
+  },
+  input: {
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 5,
+    marginVertical: 15,
+    marginHorizontal: 15
+  }
 });
